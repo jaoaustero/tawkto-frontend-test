a4d0b6ba1b247ec0708e833f4eb8b39fe96e68e3
Jerald Austero <jerald.austero0229@gmail.com>
2020-07-16 01:20:26 +0800

    🔎 SEO Meta
    We added base meta tags using vue-meta
    to manage the meta tags for SEO.

---
+++ src/meta.js
@@ -0,0 +1,15 @@
+export const meta = {
+    metaInfo: function ()
+    {
+        return {
+            title: 'Tawk',
+            titleTemplate: `${this.metaTitle} - %s`,
+            meta: [
+                {
+                    name: 'description',
+                    content: 'A frontend home test for Tawk.to company.',
+                },
+            ],
+        };
+    },
+};
