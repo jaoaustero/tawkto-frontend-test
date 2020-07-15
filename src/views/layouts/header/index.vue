d5214d24a533e62600864b704763652067621ac6
Jerald Austero <jerald.austero0229@gmail.com>
2020-07-16 01:25:39 +0800

    🚩 Update content
    We update the content of the header layout.

---
+++ src/views/layouts/header/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="t-header-wrapper">
+        <div>
+            <div>
+                <h1 class="t-text-title
+                    t-margin-remove">
+                    Knowledge base doesn't have to be boring
+                </h1>
+                <p class="t-margin-remove-bottom">
+                    Everything you need to Manage your Messaging
+                </p>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader',
+};
+</script>
