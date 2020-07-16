cbb111f79a42794d59d38269aa92241dde04c647
Jerald Austero <jerald.austero0229@gmail.com>
2020-07-16 10:34:19 +0800

    🚩 Add base stores
    We import and use the base vuex stores.

--- src/views/home/store.js
+++ src/views/home/store.js
@@ -1,1 +1,29 @@
-// Store
+// Base store
+import { baseStates, baseMutations, baseActions, baseGetters } from '@/stores/base';
+
+/**
+ * A home page state.
+ */
+export const home = {
+    namespaced: true,
+
+    state:
+    {
+        ...baseStates,
+    },
+
+    mutations:
+    {
+        ...baseMutations,
+    },
+
+    actions:
+    {
+        ...baseActions,
+    },
+
+    getters:
+    {
+        ...baseGetters,
+    },
+};
