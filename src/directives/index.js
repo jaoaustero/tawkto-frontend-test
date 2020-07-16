8abbd777f9eaa6dfa8e7748af0a8f8bf2fabf088
Jerald Austero <jerald.austero0229@gmail.com>
2020-07-16 19:02:38 +0800

    ✨ DIrective Auto-register
    We added webpack script for auto-register
    of the directives.

---
+++ src/directives/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+
+const requireComponent = require.context(
+    // Look for files in the current directory
+    '.',
+    // Look in subdirectories
+    true,
+    // Only include prefixed .vue files
+    // Include any .js files that are not this file or a index.js
+    /^((?!index|\.js\.).)*\.js$/
+);
+
+// For each matching file name...
+requireComponent.keys().forEach((fileName) =>
+{
+    // Get the component config
+    const componentConfig = requireComponent(fileName);
+
+    // Get the PascalCase version of the component name
+    const componentName = upperFirst(
+        camelCase(
+            fileName
+                // get files on the folder
+                .split('/')[2]
+                // Remove the "./_" from the beginning
+                .replace(/^\.\/_/, '')
+                // Remove the file extension from the end
+                .replace(/\.\w+$/, '')
+        )
+    );
+    // Globally register the component
+    Vue.directive(componentName, componentConfig.default || componentConfig);
+});
