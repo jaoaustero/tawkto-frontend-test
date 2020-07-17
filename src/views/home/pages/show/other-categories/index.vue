0a45ad946737c3b9f20afefd25682316537cdb76
Jerald Austero <jerald.austero0229@gmail.com>
2020-07-17 22:14:48 +0800

    🚩 Category Item Carousel
    We created a carousel for categories and
    remove the currently active.

--- src/views/home/pages/show/other-categories/index.vue
+++ src/views/home/pages/show/other-categories/index.vue
@@ -7,16 +7,45 @@
             </p>
         </div>
 
-        <div v-margin.top>
-            <t-grid>
-                <t-width
-                    v-for="(item, index) in items"
-                    :key="index">
-                    <item
-                        v-if="item.id !== $route.params.id"
-                        :item="item"/>
-                </t-width>
-            </t-grid>
+        <div class="t-flex t-flex-middle"
+            v-margin.top>
+            <div class="t-margin-medium-right">
+                <t-button
+                    :text="true"
+                    @click="prevCategory">
+                    <t-icon
+                        :type="'angle-left'"
+                        :size="'3x'"
+                        class="t-text-primary"/>
+                </t-button>
+            </div>
+
+            <div class="t-flex-1">
+                <t-grid>
+                    <t-width
+                        v-for="(item, index) in filterItems"
+                        :key="index"
+                        :xsmall="'1-1'"
+                        :small="'1-1'"
+                        :medium="'1-3'"
+                        :large="'1-3'"
+                        :xlarge="'1-3'">
+                        <item
+                            :item="item"/>
+                    </t-width>
+                </t-grid>
+            </div>
+
+            <div class="t-margin-medium-left">
+                <t-button
+                    :text="true"
+                    @click="nextCategory">
+                    <t-icon
+                        :type="'angle-right'"
+                        :size="'3x'"
+                        class="t-text-primary"/>
+                </t-button>
+            </div>
         </div>
     </div>
 </template>
@@ -56,14 +85,34 @@ export default {
         this.initialize();
     },
 
+    computed:
+    {
+        /**
+         * We filter the item by removing the current category active
+         * and returning only 3 items.
+         */
+        filterItems()
+        {
+            const data = this.items.filter((item) => item.id !== this.$route.params.id);
+
+            return data.slice(0, 3);
+        },
+    },
+
     methods:
     {
+        /**
+         * A HTTP GET method to call an endpoint for API
+         * to get the list of categories.
+         */
         async initialize()
         {
+            // Set loading
             this.loading.index = true;
 
             const response = await http.get('/categories');
 
+            // Unset loading
             this.loading.index = false;
 
             if (response.status === 200)
@@ -94,6 +143,26 @@ export default {
             {
                 alert('Internal Server Error');
             }
+        },
+
+        /**
+         * A method that will move the last index
+         * into first index.
+         */
+        prevCategory()
+        {
+            const item = this.items.pop();
+            this.items.unshift(item);
+        },
+
+        /**
+         * A method that will move the first index
+         * into last index.
+         */
+        nextCategory()
+        {
+            const item = this.items.shift();
+            this.items.push(item);
         },
     },
 };
